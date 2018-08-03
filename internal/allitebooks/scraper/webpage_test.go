db2884f2f97c213129fdd43e6bde8af853c51150
Patrick Chan <patrickchan1999@gmail.com>
2018-08-03 15:52:01 -0400

    Add test for GetLastURL() and update test for GetTotalPages()

--- internal/allitebooks/scraper/webpage_test.go
+++ internal/allitebooks/scraper/webpage_test.go
@@ -2,12 +2,22 @@ package scraper
 
 import "testing"
 
+func TestGetLastURL(t *testing.T) {
+	lastURL, err := GetLastURL("http://www.allitebooks.com")
+	if err != nil {
+		t.Error(err)
+	}
+	if lastURL != "http://www.allitebooks.com/powershell-for-sql-server-essentials/" {
+		t.Error("The scraped last url does not match!")
+	}
+}
+
 func TestGetTotalPages(t *testing.T) {
 	pages, err := GetTotalPages("http://www.allitebooks.com")
 	if err != nil {
 		t.Error(err)
 	}
-	if pages != 769 {
-		t.Error("The scraped page count does not matched!")
+	if pages != 770 {
+		t.Error("The scraped page count does not match!")
 	}
 }
