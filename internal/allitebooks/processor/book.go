1408e1850f4f816edb1af470c0d506e3ac4a05ed
Patrick Chan <patrickchan1999@gmail.com>
2018-08-03 21:11:00 -0400

    Refactor ProcessBook into smaller functions

--- internal/allitebooks/processor/book.go
+++ internal/allitebooks/processor/book.go
@@ -4,34 +4,50 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/PGo-Projects/bore/internal/allitebooks/utils"
 	"github.com/PGo-Projects/signalhandler/pkg/signalhandler"
+	tm "github.com/buger/goterm"
 )
 
 func ProcessBook(h *signalhandler.SignalHandler, title string, pdfLink string, category string, summary string) (err error) {
-	return h.WithSignalBlockedAndSignalMessage(func() error {
-		filename := path.Join(category, title+".pdf")
-		txtFilename := path.Join(category, title+".txt")
-		err = utils.CreateDirIfNotExist(category)
-		if err != nil {
-			return err
-		}
+	workFunc := func() error {
+		return processBook(title, pdfLink, category, summary)
+	}
+	messageFunc := func() {
+		tm.MoveCursor(1, 5)
+		tm.Printf("%s\r", strings.Repeat(" ", tm.Width()))
+		tm.Flush()
+		tm.MoveCursor(1, 4)
+		tm.Printf("%s\r", strings.Repeat(" ", tm.Width()))
+		tm.MoveCursor(1, 4)
+		tm.Print("   ", tm.Color("Finishing up!", tm.BLUE))
+		tm.Flush()
+	}
+	return h.WithSignalBlockedAndSignalMessageFunc(workFunc, messageFunc)
+}
 
-		pdf, err := utils.GetFile(pdfLink)
-		if err != nil {
-			return err
-		}
-
-		err = ioutil.WriteFile(filename, pdf, 0644)
-		if err != nil {
-			return err
-		}
-		err = ioutil.WriteFile(txtFilename, []byte(summary), 0644)
-		if err != nil {
-			os.Remove(filename)
-		}
+func processBook(title string, pdfLink string, category string, summary string) error {
+	filename := path.Join(category, title+".pdf")
+	txtFilename := path.Join(category, title+".txt")
+	err := utils.CreateDirIfNotExist(category)
+	if err != nil {
 		return err
-	},
-		"Finishing Up!")
+	}
+
+	pdf, err := utils.GetFile(pdfLink)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filename, pdf, 0644)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(txtFilename, []byte(summary), 0644)
+	if err != nil {
+		os.Remove(filename)
+	}
+	return err
 }
