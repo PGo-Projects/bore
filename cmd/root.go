778f294bad70df334a38f0fd2b472956be1d41e0
Patrick Chan <patrickchan1999@gmail.com>
2019-09-01 15:17:11 -0400

    Fixes #2

--- cmd/root.go
+++ cmd/root.go
@@ -25,10 +25,23 @@ func init() {
 	RootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file (default is config.yaml)")
 }
 
+func mustEnsureFileExists(path string) {
+	_, err := os.Stat(path)
+	if err != nil {
+		_, err := os.Create(path)
+		if err != nil {
+			utils.DisplayMessage("Unable to read config: "+err.Error(), tm.RED)
+			os.Exit(1)
+		}
+	}
+}
+
 func initConfig() {
 	if configFile != "" {
+		mustEnsureFileExists(configFile)
 		viper.SetConfigFile(configFile)
 	} else {
+		mustEnsureFileExists("config.yaml")
 		viper.AddConfigPath(".")
 		viper.SetConfigName("config")
 		viper.SetConfigType("yaml")
